4cb1687342368f0e1971e2d8cf08638e2fe7ea15
BalchaBe <balcha36@yahoo.com>
2023-10-23 03:30:08 -0700

    3-safe_print_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new = [0] * list_length
-    for m in range(list_length):
+    for i in range(list_length):
         try:
-            result = my_list_1[m] / my_list_2[m]
-            new[m] = result
+            result = my_list_1[i] / my_list_2[i]
+            new[i] = result
         except ZeroDivisionError:
             print("division by 0")
         except TypeError:
