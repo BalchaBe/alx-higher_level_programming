2b5967546f36b47537186559d84b159cdf6c5343
Balcha Berhanu <136062707+BalchaBe@users.noreply.github.com>
2024-01-30 14:31:22 +0300

    error fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the body of the response of a curl request
-curl -sL "$1"
+# A Bash script that takes in a URL, sends a GET request and displays only the  body of a 200 status code response
+curl -Ls "$1"
